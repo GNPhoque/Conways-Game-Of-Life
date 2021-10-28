983f6bd25fc4679667ec9323605791a9e129fc42
Phoque <lafont_jordan@hotmail.fr>
2021-10-28 11:43:37 +0200

    Modified camera orthographic size
    All cells are now contained in the screen

--- Assets/Scripts/CellGrid.cs
+++ Assets/Scripts/CellGrid.cs
@@ -24,11 +24,24 @@ public class CellGrid : MonoBehaviour
 		{
 			for (int col = 0; col < columns; col++)
 			{
-				grid[row, col] = Instantiate(cell, t, false);
+				grid[row, col] = Instantiate(cell, t);
 				grid[row, col].transform.position += new Vector3(row + row * padding, col + col * padding);
+			Debug.Log(new Vector3(row + row * padding, col + col * padding));
 			}
 		}
 		Vector3 cameraPos = grid[rows - 1, columns - 1].transform.position / 2;
-		Camera.main.transform.position = new Vector3(cameraPos.x, cameraPos.y, -10f); ;
+			Debug.Log(grid[rows - 1, columns - 1].transform.position);
+			Debug.Log(cameraPos);
+		Camera.main.transform.position = new Vector3(cameraPos.x, cameraPos.y, -10f);
+		if (cameraPos.x > cameraPos.y * 16f / 9f)
+		{
+			Debug.Log("in if");
+			Camera.main.orthographicSize = ((cameraPos.x) + 0.5f + (2 * padding)) / (16f / 9f);
+		}
+		else
+		{
+			Debug.Log("in else");
+			Camera.main.orthographicSize = cameraPos.y + 0.5f + 2 * padding;
+		}
 	}
 }
